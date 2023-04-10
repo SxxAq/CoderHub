4d293ea2fbc57d9b5cd2f698cc863d1cb210d220
SxxAq <aqueel192@gmail.com>
2023-04-11 03:07:32 +0530

    added navbar scroll color change

---
+++ script.js
@@ -0,0 +1,6 @@
+//============Change Navbar style on scroll==============
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
