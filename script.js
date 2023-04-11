c30b127e5a9bb13b8cc5f414da6bafae8bbe4344
SxxAq <aqueel192@gmail.com>
2023-04-12 02:53:08 +0530

    added courses and Faqs

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+// -----------------Faq toggle-----------------------
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    // -------------------change icon--------------------------------------
+    const icon = faq.querySelector(".faq__icon i ");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
