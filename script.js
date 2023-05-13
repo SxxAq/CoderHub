630c82e46cb604deb1548e2976a7d45d4fdc9000
SxxAq <aqueel192@gmail.com>
2023-05-13 23:32:46 +0530

    Tab responsiveness

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ faqs.forEach((faq) => {
     }
   });
 });
+
+// =================Nav-Btn Toggle===============
+const menu=document.querySelector('.nav__menu')
+const menuBtn=document.querySelector('#open-menu-btn');
+const closeBtn=document.querySelector('#close-menu-btn');
+menuBtn.addEventListener('click', ()=>{
+    menu.style.display='block';
+    closeBtn.style.display="inline-block"
+    menuBtn.style.display="none"
+})
+closeBtn.addEventListener('click', ()=>{
+    menu.style.display='none';
+    closeBtn.style.display="none"
+    menuBtn.style.display="inline-block"
+})
